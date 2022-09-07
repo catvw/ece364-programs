9019593dfdcaf0fdf4f4bba9a21a44bb6b33c46d
Catherine Van West <cat@vanwestco.com>
2022-09-07 17:15:46 -0400

    Create list of primes

---
+++ p1/hash/primes.h
@@ -0,0 +1,29 @@
+/* Primes list for the hash table.
+ * See primes.c++ for the gory details.
+ */
+
+#include <stdint.h>
+
+namespace primes {
+
+extern constexpr const size_t list[];
+extern constexpr const size_t count;
+
+} /* ~namespace primes */
+
+/*
+Copyright (C) 2022  Catherine Van West
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
