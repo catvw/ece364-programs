66df195d53472a721d14c478f5100af0a4cf4494
Catherine Van West <cat@vanwestco.com>
2022-09-11 19:08:33 -0400

    Draft checker

---
+++ p1/spell/checker.cpp
@@ -0,0 +1,67 @@
+#include "spell/checker.h"
+
+#include <sstream>
+
+checker::checker(const std::string& wordlist) {
+	std::istringstream is(wordlist);
+	word w;
+	bool more_words = true;
+
+	while (more_words) {
+		more_words = !(is >> w).fail();
+		dict.add_word(w);
+	}
+}
+
+std::string checker::check(const std::string& document) {
+	std::istringstream is_doc(document);
+	std::ostringstream objections;
+
+	bool more_lines = true;
+	size_t line_number = 1; // probably start from 1?
+
+	// go through the input line-by-line and check each one
+	std::string line;
+	while (more_lines) {
+		more_lines = !std::getline(is_doc, line).fail();
+
+		std::istringstream is_line(line);
+		word w;
+		bool more_words = true;
+		while (more_words) {
+			more_words = !(is_line >> w).fail();
+			object_to(w, line, objections);
+		}
+	}
+
+	return objections.str()
+}
+
+void checker::object_to(const word& w, size_t line, std::ostream& obj) {
+	if (w.str().length() > max_word_length) {
+		obj << "Long word at line " << line
+		    << ", starts " << w.substr(0, max_word_length);
+		    << '\n';
+	} else if (!dict.contains(w)) {
+		obj << "Unknown word at line " << line
+		    << ": " << w.str();
+		    << '\n';
+	}
+}
+
+/*
+Copyright (C) 2022  Catherine Van West
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
---
+++ p1/spell/checker.h
@@ -0,0 +1,57 @@
+/* Spellchecker class for the spellchecker. Sort of redundant, but... that's
+   what this is. */
+
+#ifndef CHECKER_H
+#define CHECKER_H
+
+#include "spell/dictionary.h"
+#include "spell/word.h"
+
+#include <cstdint>
+#include <string>
+#include <ostream>
+
+class checker {
+public:
+	/* Construct a spellchecker with the given wordlist. Every "word" in the
+	   wordlist will be read into the checker's internal dictionary. */
+	checker(const std::string&);
+
+	/* Check the given document against the dictionary, and report all the
+	   violations. */
+	std::string check(const std::string&);
+
+	/* Set the maximum word length to consider a valid word. This does not
+	   affect the internal dictionary, so feel free to alter it at will.
+	   Negative values will disable length checking.
+
+	   Respectfully, why do we *need* a maximum word length? */
+	void set_max_word_length(ssize_t);
+
+private:
+	/* Write objections to the given word on the given line to the given
+	   stream. */
+	void object_to(const word&, size_t, std::ostream&);
+
+	dictionary dict;
+	ssize_t max_word_length;
+};
+
+#endif
+
+/*
+Copyright (C) 2022  Catherine Van West
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
