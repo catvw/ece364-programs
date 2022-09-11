ba05372dcaa98f852412db18969c7dacd416ef0c
Catherine Van West <cat@vanwestco.com>
2022-09-11 18:34:37 -0400

    Draft dictionary (lol)

---
+++ p1/spell/dictionary.cpp
@@ -0,0 +1,10 @@
+#include "spell/dictionary.h"
+
+void dictionary::add_word(const word& w) {
+	// you never said what we should do if the rehash fails, so...
+	words.insert(w.str(), nullptr);
+}
+
+bool dictionary::contains(const word& w) {
+	return words.contains(w.str());
+}
