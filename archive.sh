b5dc6d90d86043099b6252c82fc7e64ed77cc1a5
Catherine Van West <cat@vanwestco.com>
2022-09-14 16:51:56 -0400

    Rewrite with different rule priorities

--- archive.sh
+++ archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # archive program 1
-tar acvf p1.tar.gz p1/Makefile p1/.subdeps p1/test.sh p1/main.cpp p1/hash/ p1/spell/ p1/ref/
+tar acvf p1.tar.gz p1/Makefile p1/test.sh p1/main.cpp p1/hash/ p1/spell/ p1/ref/
 
 #Copyright (C) 2022  Catherine Van West
 #
