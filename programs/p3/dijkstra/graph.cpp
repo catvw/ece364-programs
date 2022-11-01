a20235c2b05dbcd17fa455cbeed86a7e0367a1df
Catherine Van West <cat@vanwestco.com>
2022-11-01 19:50:59 -0400

    Add license comments

--- programs/p3/dijkstra/graph.cpp
+++ programs/p3/dijkstra/graph.cpp
@@ -49,3 +49,20 @@ void graph::vertex::add_edge(const string& to, unsigned int cost) {
 	// note: should probably check if this edge already exists, but...
 	edges.push_back(move(edge{to, cost}));
 }
+
+/*
+Copyright (C) 2022  Catherine Van West
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
