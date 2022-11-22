6db6c2b503b988c34b88bd395fcda46f6874dda5
Catherine Van West <cat@vanwestco.com>
2022-11-21 20:54:07 -0500

    Fix fuzzing hopefully permanently

--- programs/p4/fuzz.py
+++ programs/p4/fuzz.py
@@ -38,22 +38,43 @@ def screw_up(merge):
 	new_letter = merge[i].lower()
 	while new_letter == merge[i].lower():
 		new_letter = random_letter()
+		if random.random() > .5:
+			new_letter = new_letter.upper()
 	merge[i] = new_letter
 
 	return ''.join(merge)
 
-random.seed()
-with open('ref/fuzz_in', 'w') as infile, open('ref/fuzz_out', 'w') as outfile:
-	for _ in range(MERGES):
-		a = gen_string(LENGTH)
-		b = gen_string(LENGTH)
-		merge = random_merge(a, b)
-		expected_out = merge
+# look for lowercase runs that can be pushed back and do so
+def shove_runs(merge):
+	# I *know* -- this is not efficient... but it's fast enough!
+	for _ in range(len(merge)):
+		i = 0
+		while i < len(merge):
+			j = len(merge) - i
+			while j > 0:
+				run = merge[i:(i + j)]
+				next_sect = merge[(i + j):(i + 2*j)]
+				if run.islower() and next_sect.isupper() and run == next_sect.lower():
+					# swap the two
+					merge = f'{merge[:i]}{next_sect}{run}{merge[(i + 2*j):]}'
+				j = j - 1
+			i = i + 1
 
-		is_error = random.random() < ERROR_RATE
-		if is_error:
-			merge = screw_up(merge)
-			expected_out = '*** NOT A MERGE ***'
+	return merge
 
-		infile.write(f'{a}\n{b}\n{merge.lower()}\n')
-		outfile.write(f'{expected_out}\n')
+if __name__ == "__main__":
+	random.seed()
+	with open('ref/fuzz_in', 'w') as infile, open('ref/fuzz_out', 'w') as outfile:
+		for _ in range(MERGES):
+			a = gen_string(LENGTH)
+			b = gen_string(LENGTH)
+			merge = random_merge(a, b)
+			expected_out = shove_runs(merge)
+
+			is_error = random.random() < ERROR_RATE
+			if is_error:
+				merge = screw_up(merge)
+				expected_out = '*** NOT A MERGE ***'
+
+			infile.write(f'{a}\n{b}\n{merge.lower()}\n')
+			outfile.write(f'{expected_out}\n')
