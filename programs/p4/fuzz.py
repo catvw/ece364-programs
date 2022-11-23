d582b4c4d89e55854d362d586fb8732a201b13ac
Catherine Van West <cat@vanwestco.com>
2022-11-23 16:50:37 -0500

    License fuzzer

--- programs/p4/fuzz.py
+++ programs/p4/fuzz.py
@@ -82,3 +82,18 @@ if __name__ == "__main__":
 			infile.write(f'{a}\n{b}\n{merge.lower()}\n')
 			outfile.write(f'{expected_out}\n')
 		print(' generated.')
+
+# Copyright (C) 2022  Catherine Van West
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
